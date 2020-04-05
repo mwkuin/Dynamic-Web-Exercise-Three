c243d7995e3ba68e7ee59e5aaa903a1ff974bc38
mwkuin <43649987+mwkuin@users.noreply.github.com>
2020-04-05 14:10:28 -0400

    second commit

--- exercise-3/app.js
+++ exercise-3/app.js
@@ -3,6 +3,16 @@ const express = require('express')
 const app= express();
 const port = 4000;
 
-app.get('/', (req,res) => res.send("Hello World"));
+const indexRoute = require('./routes/index.js');
+const aboutRoute = require('./routes/about.js');
 
+
+//serve static files
+app.use('/static', express.static('public'));
+
+//routing in express
+app.use('/', indexRoute);
+app.use('/about', aboutRoute);
+
+// app.get('/', (req,res) => res.send("Hello World"));
 app.listen(port, () => console.log('Hello World Running!'));
